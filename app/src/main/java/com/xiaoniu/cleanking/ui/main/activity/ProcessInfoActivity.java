8d2a692c4ebb30f59a329c447fd48b65e9d5107c
tie <493820064@qq.com>
2019-07-05 17:13:09 +0800

    添加硬件降温功能

---
+++ app/src/main/java/com/xiaoniu/cleanking/ui/main/activity/ProcessInfoActivity.java
@@ -0,0 +1,66 @@
+package com.xiaoniu.cleanking.ui.main.activity;
+
+import android.os.Build;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.alibaba.android.arouter.facade.annotation.Route;
+import com.xiaoniu.cleanking.R;
+import com.xiaoniu.cleanking.app.RouteConstants;
+import com.xiaoniu.cleanking.base.SimpleActivity;
+import com.xiaoniu.cleanking.ui.main.adapter.ProcessInfoAdapter;
+import com.xiaoniu.cleanking.ui.main.bean.FirstJunkInfo;
+import com.xiaoniu.cleanking.widget.statusbarcompat.StatusBarCompat;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+/**
+ * @author Administrator
+ */
+@Route(path = RouteConstants.PROCESS_INFO_ACTIVITY)
+public class ProcessInfoActivity extends SimpleActivity {
+
+    @BindView(R.id.tv_title)
+    TextView mTextTitle;
+    @BindView(R.id.recyclerView)
+    RecyclerView mRecyclerView;
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_process_info;
+    }
+
+    @Override
+    protected void initView() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            StatusBarCompat.setStatusBarColor(this, getResources().getColor(R.color.color_4690FD), true);
+        } else {
+            StatusBarCompat.setStatusBarColor(this, getResources().getColor(R.color.color_4690FD), false);
+        }
+
+        List<FirstJunkInfo> list = PhoneCoolingActivity.mRunningProcess;
+
+        if (list != null) {
+            mTextTitle.setText(list.size() + "个运行的应用");
+        }
+
+        initData(list);
+    }
+
+    private void initData(List<FirstJunkInfo> list) {
+        ProcessInfoAdapter processIconAdapter = new ProcessInfoAdapter();
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(processIconAdapter);
+        processIconAdapter.setData(list);
+    }
+
+    @OnClick({R.id.img_back})
+    public void onBackPress(View view) {
+        finish();
+    }
+}
