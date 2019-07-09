3f86e1f67c3e565c1e8f6b5d2cf5de324a52e5d2
Lang.Chen <snowf07@163.com>
2019-07-09 14:20:46 +0800

    --提交手机瘦身UI页

---
+++ app/src/main/java/com/xiaoniu/cleanking/ui/main/activity/PhoneThinActivity.java
@@ -0,0 +1,33 @@
+package com.xiaoniu.cleanking.ui.main.activity;
+
+import com.xiaoniu.cleanking.R;
+import com.xiaoniu.cleanking.app.injector.component.ActivityComponent;
+import com.xiaoniu.cleanking.base.BaseActivity;
+
+/**
+ * 手机瘦身
+ * Created by lang.chen on 2019/7/9
+ */
+public class PhoneThinActivity extends BaseActivity {
+
+
+    @Override
+    public void inject(ActivityComponent activityComponent) {
+
+    }
+
+    @Override
+    public void netError() {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_phone_thin;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
