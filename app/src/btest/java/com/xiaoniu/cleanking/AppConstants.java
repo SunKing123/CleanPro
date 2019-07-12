9249f69ad95d2bb26a31c666d7019dcaf5ecd3b7
Lang.Chen <snowf07@163.com>
2019-07-12 14:17:19 +0800

    --埋点url更改

--- app/src/btest/java/com/xiaoniu/cleanking/AppConstants.java
+++ app/src/btest/java/com/xiaoniu/cleanking/AppConstants.java
@@ -18,7 +18,9 @@ public class AppConstants {
     public static String GuanJia_Host2 = "http://testloan.mydkguanjia.com";//贷款管家接口域名
     public static String Action_Base_Host = "http://testosplatform.51huihuahua.com";//活动接口域名
     public static String AGGREMENT_H5_HOST = "http://testh5.51huihuahua.com";//相关协议域名
-    public static String BIGDATA_MD = "http://testwkdataprobe.51huihuahua.com/wkdataprobe/apis";//大数据买点
+    //    public static String BIGDATA_MD = "http://testwkdataprobe.51huihuahua.com/wkdataprobe/apis";//大数据买点
+
+    public static String BIGDATA_MD = "http://192.168.85.135:9050/wkdataprobe/apis";//大数据买点
 
     public static String OSS_ACCESS_KEY_ID = "LTAIWVBu27n5Vw0d";
     public static String OSS_ACCESS_KEY_SECRET = "18i7N4uSuLMlICX3Lk7r98uVOWPz4b";
