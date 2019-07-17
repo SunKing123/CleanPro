d213b53eec057a0260b70920828e6a27eccfda2c
zhangshuyong <zhangshuyong@xiaoniu.com>
2019-07-17 20:38:37 +0800

    新大数据域名

--- app/src/prod/java/com/xiaoniu/cleanking/AppConstants.java
+++ app/src/prod/java/com/xiaoniu/cleanking/AppConstants.java
@@ -9,7 +9,7 @@ public class AppConstants {
     //生产环境服务器
     public static String Base_Host = "https://clsystem.wukongclean.com";
     public static String Base_H5_Host = "https://wkqlapph5.wukongclean.com";//H5路径
-    public static String BIGDATA_MD = "https://wkdataprobe.51huihuahua.com/wkdataprobe/apis";//大数据买点
+    public static String BIGDATA_MD = "https://wkdataprobe.wukongclean.com";//大数据买点
 
     //之前的埋点sdk接口
     public static String Base_Big_Data = "http://testhhh.xnshandai.net";//大数据接口路径
