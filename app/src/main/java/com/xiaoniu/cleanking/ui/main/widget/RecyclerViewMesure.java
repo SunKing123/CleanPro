37671ef824c60603eb022ca7ef484ad64bf5106e
Lang.Chen <snowf07@163.com>
2019-08-07 16:09:28 +0800

    --解决高度显示不全的问题

---
+++ app/src/main/java/com/xiaoniu/cleanking/ui/main/widget/RecyclerViewMesure.java
@@ -0,0 +1,36 @@
+package com.xiaoniu.cleanking.ui.main.widget;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+/**
+ * Created by lang.chen on 2019/8/7
+ */
+public class RecyclerViewMesure extends RecyclerView {
+
+    public RecyclerViewMesure(Context context) {
+        super(context);
+    }
+
+
+    public RecyclerViewMesure(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RecyclerViewMesure(Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    /**
+     * 设置不滚动
+     */
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+    {
+        int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2,
+                MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, expandSpec);
+
+    }
+}
