44bfcb94ffcafa0356363a6c4ae8a3be5b1fb9ae
wangdeshun <Wang123@>
2019-08-20 17:41:21 +0800

    优化已清理完成，再次点击显示分享页

--- app/src/main/java/com/xiaoniu/cleanking/ui/tool/wechat/activity/WechatCleanedResultActivity.java
+++ app/src/main/java/com/xiaoniu/cleanking/ui/tool/wechat/activity/WechatCleanedResultActivity.java
@@ -54,6 +54,8 @@ public class WechatCleanedResultActivity extends SimpleActivity {
         mCleanAnimView.setVisibility(View.VISIBLE);
 //        mCleanAnimView.startTopAnim(true);
         mCleanAnimView.setListener(() -> finish());
+
+        mCleanAnimView.showWebView();
     }
 
     /**
