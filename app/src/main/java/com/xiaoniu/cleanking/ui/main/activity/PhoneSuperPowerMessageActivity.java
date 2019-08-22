319dbdf9eb4c08a330638f2c3b31e9d73423f9f9
wangdeshun <Wang123@>
2019-08-22 13:54:54 +0800

    添加超强省电通知页面

---
+++ app/src/main/java/com/xiaoniu/cleanking/ui/main/activity/PhoneSuperPowerMessageActivity.java
@@ -0,0 +1,17 @@
+package com.xiaoniu.cleanking.ui.main.activity;
+
+import com.xiaoniu.cleanking.R;
+import com.xiaoniu.cleanking.base.SimpleActivity;
+
+public class PhoneSuperPowerMessageActivity extends SimpleActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_phone_super_power_messge;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
