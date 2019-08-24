5156d8cec6063c92b3a4615ae7e8c91ceae1f9f3
wangdeshun <Wang123@>
2019-08-24 18:50:29 +0800

    更新

--- app/src/main/java/com/xiaoniu/cleanking/ui/main/activity/NotificationBarMessageActivity.java
+++ app/src/main/java/com/xiaoniu/cleanking/ui/main/activity/NotificationBarMessageActivity.java
@@ -3,6 +3,7 @@ package com.xiaoniu.cleanking.ui.main.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.xiaoniu.cleanking.R;
@@ -15,6 +16,8 @@ public class NotificationBarMessageActivity extends BaseActivity {
 
     private FrameLayout mFlClean;
     private TextView mTvClean;
+    private TextView mTvTitle;
+    private ImageView mIvRight;
     @Override
     protected int getLayoutResId() {
         return R.layout.activity_notifaction_message;
@@ -22,21 +25,26 @@ public class NotificationBarMessageActivity extends BaseActivity {
 
     @Override
     protected void initVariable(Intent intent) {
-        setTitle(getString(R.string.tool_notification_clean));
+        hideToolBar();
     }
 
     @Override
     protected void initViews(Bundle savedInstanceState) {
         mFlClean = findViewById(R.id.fl_clean_layout);
         mTvClean = findViewById(R.id.tv_clean);
+        mTvTitle = findViewById(R.id.tv_title);
+        mIvRight = findViewById(R.id.iv_set);
+
+        mTvTitle.setText(getString(R.string.tool_notification_clean));
 
     }
 
     @Override
     protected void setListener() {
         mTvClean.setOnClickListener(v -> {
-
+            finish();
         });
+        mIvRight.setOnClickListener(v -> startActivity(null));
     }
 
     @Override
