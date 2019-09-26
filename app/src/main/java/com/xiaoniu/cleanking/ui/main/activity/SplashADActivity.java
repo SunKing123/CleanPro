856eb91ccd64d78b61ece22c1b81dcddb65e3cd3
wangdeshun <Wang123@>
2019-09-26 13:11:38 +0800

    优化闪屏代码

--- app/src/main/java/com/xiaoniu/cleanking/ui/main/activity/SplashADActivity.java
+++ app/src/main/java/com/xiaoniu/cleanking/ui/main/activity/SplashADActivity.java
@@ -80,6 +80,11 @@ public class SplashADActivity extends BaseActivity<SplashPresenter> implements S
     private Disposable mSubscription;
 
     @Override
+    protected int getLayoutId() {
+        return R.layout.activity_splash_ad;
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mPresenter.getSwitchInfoList();
@@ -223,19 +228,20 @@ public class SplashADActivity extends BaseActivity<SplashPresenter> implements S
         fetchSplashADTime = System.currentTimeMillis();
 
         //后台控制是否显示开关
-        if (AppHolder.getInstance().getSwitchInfoList() == null)
+        if (AppHolder.getInstance() == null || AppHolder.getInstance().getSwitchInfoList() == null || AppHolder.getInstance().getSwitchInfoList().getData() == null) {
             jumpActivity();
-
-        for (SwitchInfoList.DataBean switchInfoList : AppHolder.getInstance().getSwitchInfoList().getData()) {
-            if (PositionId.SPLASH_ID.equals(switchInfoList.getId())) {
-                if (!switchInfoList.isIsOpen()) {
-                    //广告
-                    splashAD = new SplashAD(activity, skipContainer, appId, posId, adListener, fetchDelay);
-                    splashAD.fetchAndShowIn(adContainer);
-                    return;
-                } else {
-                    jumpActivity();
-                    return;
+        }else {
+            for (SwitchInfoList.DataBean switchInfoList : AppHolder.getInstance().getSwitchInfoList().getData()) {
+                if (PositionId.SPLASH_ID.equals(switchInfoList.getId())) {
+                    if (switchInfoList.isIsOpen()) {
+                        //广告
+                        splashAD = new SplashAD(activity, skipContainer, appId, posId, adListener, fetchDelay);
+                        splashAD.fetchAndShowIn(adContainer);
+                        return;
+                    } else {
+                        jumpActivity();
+                        return;
+                    }
                 }
             }
         }
@@ -317,11 +323,6 @@ public class SplashADActivity extends BaseActivity<SplashPresenter> implements S
     protected void onPause() {
         super.onPause();
         canJump = false;
-    }
-
-    @Override
-    protected int getLayoutId() {
-        return R.layout.activity_splash_ad;
     }
 
     @Override
