2e6fb05ab39b19915aaa4e3478c81944c5ea3371
deshunWang <wangdeshun_com@163.com>
2019-10-04 10:39:56 +0800

    更新首页布局

---
+++ app/src/main/java/com/xiaoniu/cleanking/ui/newclean/fragment/NewCleanMainFragment.java
@@ -0,0 +1,225 @@
+package com.xiaoniu.cleanking.ui.newclean.fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.xiaoniu.cleanking.R;
+import com.xiaoniu.cleanking.app.AppManager;
+import com.xiaoniu.cleanking.app.RouteConstants;
+import com.xiaoniu.cleanking.app.injector.component.FragmentComponent;
+import com.xiaoniu.cleanking.base.AppHolder;
+import com.xiaoniu.cleanking.base.BaseFragment;
+import com.xiaoniu.cleanking.ui.main.activity.FileManagerHomeActivity;
+import com.xiaoniu.cleanking.ui.main.activity.MainActivity;
+import com.xiaoniu.cleanking.ui.main.activity.NewsActivity;
+import com.xiaoniu.cleanking.ui.main.activity.PhoneAccessActivity;
+import com.xiaoniu.cleanking.ui.main.activity.PhoneSuperPowerActivity;
+import com.xiaoniu.cleanking.ui.main.activity.PhoneThinActivity;
+import com.xiaoniu.cleanking.ui.main.config.SpCacheConfig;
+import com.xiaoniu.cleanking.ui.main.presenter.CleanMainPresenter;
+import com.xiaoniu.cleanking.ui.main.widget.SPUtil;
+import com.xiaoniu.cleanking.ui.newclean.activity.NewCleanFinishActivity;
+import com.xiaoniu.cleanking.ui.newclean.activity.NowCleanActivity;
+import com.xiaoniu.cleanking.ui.tool.notify.manager.NotifyCleanManager;
+import com.xiaoniu.cleanking.ui.tool.qq.activity.QQCleanHomeActivity;
+import com.xiaoniu.cleanking.ui.tool.qq.util.QQUtil;
+import com.xiaoniu.cleanking.ui.tool.wechat.activity.WechatCleanHomeActivity;
+import com.xiaoniu.cleanking.ui.usercenter.activity.PermissionActivity;
+import com.xiaoniu.cleanking.utils.AndroidUtil;
+import com.xiaoniu.cleanking.utils.update.PreferenceUtil;
+import com.xiaoniu.common.utils.StatisticsUtils;
+import com.xiaoniu.common.utils.ToastUtils;
+
+import butterknife.OnClick;
+
+public class NewCleanMainFragment extends BaseFragment<CleanMainPresenter> {
+
+    private long firstTime;
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_new_clean_main;
+    }
+
+    @Override
+    protected void initView() {
+    }
+
+    public void startCleanNow() {
+    }
+
+    @Override
+    protected void inject(FragmentComponent fragmentComponent) {
+        fragmentComponent.inject(this);
+    }
+
+    @Override
+    public void netError() {
+
+    }
+
+    /**
+     * 立即清理
+     */
+    @OnClick(R.id.tv_now_clean)
+    public void nowClean() {
+        startActivity(NowCleanActivity.class);
+    }
+
+
+    /**
+     * 文件管理
+     */
+    @OnClick(R.id.text_wjgl)
+    public void wjgl() {
+        ((MainActivity) getActivity()).commitJpushClickTime(4);
+        StatisticsUtils.trackClick("file_clean_click", "用户在首页点击【文件清理】按钮", "home_page", "home_page");
+        startActivity(FileManagerHomeActivity.class);
+    }
+
+    /**
+     * 一键加速
+     */
+    @OnClick(R.id.text_acce)
+    public void text_acce() {
+        AppHolder.getInstance().setCleanFinishSourcePageId("boost_click");
+        AppHolder.getInstance().setOtherSourcePageId(SpCacheConfig.ONKEY);
+        ((MainActivity) getActivity()).commitJpushClickTime(2);
+        StatisticsUtils.trackClick("boost_click", "用户在首页点击【一键加速】按钮", "home_page", "home_page");
+        //保存本次清理完成时间 保证每次清理时间间隔为3分钟
+        if (PreferenceUtil.getCleanTime()) {
+            PreferenceUtil.saveCleanTime();
+            Bundle bundle = new Bundle();
+            bundle.putString("title", getString(R.string.tool_one_key_speed));
+            bundle.putString("num", "");
+            bundle.putString("unit", "");
+            startActivity(NewCleanFinishActivity.class, bundle);
+        } else {
+            Bundle bundle = new Bundle();
+            bundle.putString(SpCacheConfig.ITEM_TITLE_NAME, getString(R.string.tool_one_key_speed));
+            startActivity(PhoneAccessActivity.class, bundle);
+        }
+    }
+
+    /**
+     * 超强省电
+     */
+    @OnClick(R.id.line_shd)
+    public void line_shd() {
+        AppHolder.getInstance().setCleanFinishSourcePageId("powersave_click");
+        ((MainActivity) getActivity()).commitJpushClickTime(3);
+        AppHolder.getInstance().setOtherSourcePageId(SpCacheConfig.SUPER_POWER_SAVING);
+        startActivity(PhoneSuperPowerActivity.class);
+        StatisticsUtils.trackClick("powersave_click", "用户在首页点击【超强省电】按钮", "home_page", "home_page");
+    }
+
+    /**
+     * 新闻点击
+     */
+    @OnClick(R.id.view_news)
+    public void ViewNewsClick() {
+        StatisticsUtils.trackClick("news_click", "用户在首页点击【头条新闻热点】按钮", "home_page", "home_page");
+        startActivity(NewsActivity.class);
+    }
+
+    /**
+     * 软件管理
+     */
+    @OnClick(R.id.view_phone_thin)
+    public void ViewPhoneThinClick() {
+
+        Intent intent = new Intent(getActivity(), PhoneThinActivity.class);
+        intent.putExtra(SpCacheConfig.ITEM_TITLE_NAME, getString(R.string.tool_soft_manager));
+        startActivity(intent);
+        StatisticsUtils.trackClick("app_manage_click", "用户在首页点击【软件管理】按钮", "home_page", "home_page");
+    }
+
+    /**
+     * QQ专清
+     */
+    @OnClick(R.id.view_qq_clean)
+    public void ViewQQCleanClick() {
+        AppHolder.getInstance().setOtherSourcePageId(SpCacheConfig.QQ_CLEAN);
+        ((MainActivity) getActivity()).commitJpushClickTime(7);
+        StatisticsUtils.trackClick("qqclean_click", "“用户在首页点击【qq专清】按钮", "home_page", "home_page");
+        if (!AndroidUtil.isAppInstalled(SpCacheConfig.QQ_PACKAGE)) {
+            ToastUtils.showShort(R.string.tool_no_install_qq);
+            return;
+        }
+        if (QQUtil.audioList != null)
+            QQUtil.audioList.clear();
+        if (QQUtil.fileList != null)
+            QQUtil.fileList.clear();
+        startActivity(QQCleanHomeActivity.class);
+    }
+
+    /**
+     * 权限设置
+     */
+    @OnClick(R.id.iv_permission)
+    public void onClick() {
+        startActivity(new Intent(getContext(), PermissionActivity.class));
+        StatisticsUtils.trackClick("Triangular_yellow_mark_click", "三角黄标", AppHolder.getInstance().getSourcePageId(), "permission_page");
+    }
+
+    /**
+     * 微信专清
+     */
+    @OnClick(R.id.line_wx)
+    public void mClickWx() {
+        AppHolder.getInstance().setCleanFinishSourcePageId("wxclean_click");
+        AppHolder.getInstance().setOtherSourcePageId(SpCacheConfig.WETCHAT_CLEAN);
+
+        ((MainActivity) getActivity()).commitJpushClickTime(5);
+        StatisticsUtils.trackClick("wxclean_click", "用户在首页点击【微信专清】按钮", "home_page", "home_page");
+        if (!AndroidUtil.isAppInstalled(SpCacheConfig.CHAT_PACKAGE)) {
+            ToastUtils.showShort(R.string.tool_no_install_chat);
+            return;
+        }
+        if (PreferenceUtil.getWeChatCleanTime()) {
+            // 每次清理间隔 至少3秒
+            startActivity(WechatCleanHomeActivity.class);
+        } else {
+            Bundle bundle = new Bundle();
+            bundle.putString("title", getString(R.string.tool_chat_clear));
+            bundle.putString("num", "");
+            bundle.putString("unit", "");
+            startActivity(NewCleanFinishActivity.class, bundle);
+        }
+    }
+
+    /**
+     * 通知清理
+     */
+    @OnClick(R.id.line_super_power_saving)
+    public void mClickQq() {
+        AppHolder.getInstance().setCleanFinishSourcePageId("notification_clean_click");
+        //通知栏清理
+        NotifyCleanManager.startNotificationCleanActivity(getActivity(), 0);
+        StatisticsUtils.trackClick("notification_clean_click", "用户在首页点击【通知清理】按钮", AppHolder.getInstance().getSourcePageId(), "home_page");
+    }
+
+    /**
+     * 手机降温
+     */
+    @OnClick(R.id.line_jw)
+    public void mClickJw() {
+        AppHolder.getInstance().setCleanFinishSourcePageId("cooling_click");
+        ((MainActivity) getActivity()).commitJpushClickTime(6);
+        startActivity(RouteConstants.PHONE_COOLING_ACTIVITY);
+        StatisticsUtils.trackClick("cooling_click", "用户在首页点击【手机降温】按钮", AppHolder.getInstance().getSourcePageId(), "home_page");
+    }
+
+    public void onKeyBack() {
+        long currentTimeMillis = System.currentTimeMillis();
+        if (currentTimeMillis - firstTime > 1500) {
+            Toast.makeText(getActivity(), "再按一次退出程序", Toast.LENGTH_SHORT).show();
+            firstTime = currentTimeMillis;
+        } else {
+            SPUtil.setInt(getContext(), "turnask", 0);
+            AppManager.getAppManager().AppExit(getContext(), false);
+        }
+    }
+
+}
