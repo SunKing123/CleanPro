4b085ed53b0c8fea924530a9909356a8eb4bfd52
zhengzhihao <zhengzhihao@hellogeek.com>
2019-11-13 16:33:19 +0800

    埋点url地址替换

--- app/src/prod/java/com/xiaoniu/cleanking/AppConstants.java
+++ app/src/prod/java/com/xiaoniu/cleanking/AppConstants.java
@@ -9,7 +9,7 @@ public class AppConstants {
     //生产环境服务器
     public static String Base_Host = "https://clsystem.wukongclean.com";
     public static String Base_H5_Host = "https://wkqlapph5.wukongclean.com";//H5路径
-    public static String BIGDATA_MD = "https://wkdataprobe.wukongclean.com/wkdataprobe/apis";//大数据买点
+    public static String BIGDATA_MD = "http://aidataprobe2.openxiaoniu.com/aidataprobe2/wk";//大数据买点
 
     //之前的埋点sdk接口
     public static String Base_Big_Data = "http://testhhh.xnshandai.net";//大数据接口路径
