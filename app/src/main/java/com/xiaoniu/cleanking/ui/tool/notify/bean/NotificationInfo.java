f64ec3719a7bc63dd5d077739e7fc8c83889b344
xileilei <xileilei@hellogeek.com>
2019-12-04 18:20:03 +0800

    get set设置

--- app/src/main/java/com/xiaoniu/cleanking/ui/tool/notify/bean/NotificationInfo.java
+++ app/src/main/java/com/xiaoniu/cleanking/ui/tool/notify/bean/NotificationInfo.java
@@ -13,4 +13,60 @@ public class NotificationInfo implements Serializable {
     public Bitmap icon;
     public long time;
     public PendingIntent intent;
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public String getPkg() {
+        return pkg;
+    }
+
+    public void setPkg(String pkg) {
+        this.pkg = pkg;
+    }
+
+    public CharSequence getTitle() {
+        return title;
+    }
+
+    public void setTitle(CharSequence title) {
+        this.title = title;
+    }
+
+    public CharSequence getContent() {
+        return content;
+    }
+
+    public void setContent(CharSequence content) {
+        this.content = content;
+    }
+
+    public Bitmap getIcon() {
+        return icon;
+    }
+
+    public void setIcon(Bitmap icon) {
+        this.icon = icon;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public PendingIntent getIntent() {
+        return intent;
+    }
+
+    public void setIntent(PendingIntent intent) {
+        this.intent = intent;
+    }
 }
