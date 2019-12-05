dccbee28710541b975e00841382305fbade86b94
zhengzhihao <zhengzhihao@hellogeek.com>
2019-12-05 18:13:10 +0800

    网络状态监听

--- app/src/main/java/com/xiaoniu/cleanking/app/DebugActivity.java
+++ app/src/main/java/com/xiaoniu/cleanking/app/DebugActivity.java
@@ -1,6 +1,8 @@
 package com.xiaoniu.cleanking.app;
 
+import android.app.Activity;
 import android.content.ComponentName;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.view.View;
 import android.widget.TextView;
@@ -142,13 +144,39 @@ public class DebugActivity extends BaseActivity {
         }
     }
 
-/*    *//**
-     *back the app's icon.
-     *//*
+  /*  *
+     *back the app's icon.*/
     public void backIcon(){
         PackageManager p = getPackageManager();
         ComponentName componentName = new ComponentName(this, SplashADActivity.class);
         p.setComponentEnabledSetting(componentName, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
-    }*/
+    }
 
+
+    /**
+     *  添加当活动为启动项
+     * @param cx
+     * @param name 快捷方式名称
+     *  调用示例：      ShortCutUtils.addShortcut(MainActivity.this, name.getText() != null ? name.getText().toString() : "1");
+     */
+    public static void addShortcut(Activity cx, String name) {
+        // TODO: 2017/6/25  创建快捷方式的intent广播
+        Intent shortcut = new Intent("com.android.launcher.action.INSTALL_SHORTCUT");
+        // TODO: 2017/6/25 添加快捷名称
+        shortcut.putExtra(Intent.EXTRA_SHORTCUT_NAME, name);
+        //  快捷图标是允许重复
+        shortcut.putExtra("duplicate", false);
+        // 快捷图标
+        Intent.ShortcutIconResource iconRes = Intent.ShortcutIconResource.fromContext(cx, R.mipmap.applogo);
+        shortcut.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, iconRes);
+        // TODO: 2017/6/25 我们下次启动要用的Intent信息
+        Intent carryIntent = new Intent(Intent.ACTION_MAIN);
+        carryIntent.putExtra("name", name);
+        carryIntent.setClassName(cx.getPackageName(),cx.getClass().getName());
+        carryIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        //添加携带的Intent
+        shortcut.putExtra(Intent.EXTRA_SHORTCUT_INTENT, carryIntent);
+        // TODO: 2017/6/25  发送广播
+        cx.sendBroadcast(shortcut);
+    }
 }
