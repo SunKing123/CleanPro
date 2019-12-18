72f1f9e321649f07a6f28f98e26cccd8c44035e2
xileilei <xileilei@hellogeek.com>
2019-12-18 18:01:34 +0800

    热启动null优化

--- app/src/main/java/com/xiaoniu/cleanking/ui/main/activity/SplashADHotActivity.java
+++ app/src/main/java/com/xiaoniu/cleanking/ui/main/activity/SplashADHotActivity.java
@@ -204,6 +204,7 @@ public class SplashADHotActivity extends BaseActivity<SplashHotPresenter> {
                     }
                     StatisticsUtils.customAD("ad_show", "广告展示曝光", "1", " ", "自定义广告", "hot_splash_page", "hot_splash_page", dataBean.getSwitcherName());
                     GlideUtils.loadImage(SplashADHotActivity.this, dataBean.getAdvBottomPicsDTOS().get(mBottomAdShowCount).getImgUrl(), mErrorAdIv);
+                    if (null == mErrorAdIv) return;
                     mErrorAdIv.setOnClickListener(v -> {
                         mIsAdError = true;
                         StatisticsUtils.clickAD("ad_click", "广告点击", "1", " ", "自定义广告", "hot_splash_page", "hot_splash_page", dataBean.getSwitcherName());
