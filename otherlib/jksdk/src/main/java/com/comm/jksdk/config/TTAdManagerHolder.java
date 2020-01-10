156b7851329f204543624047563c8c45783fac94
zhengzhihao <zhengzhihao@hellogeek.com>
2020-01-10 20:07:18 +0800

    穿山甲sdk升级2.8.0版本；

--- otherlib/jksdk/src/main/java/com/comm/jksdk/config/TTAdManagerHolder.java
+++ otherlib/jksdk/src/main/java/com/comm/jksdk/config/TTAdManagerHolder.java
@@ -1,14 +1,12 @@
 package com.comm.jksdk.config;
 
 import android.content.Context;
-import android.text.TextUtils;
 
 import com.bytedance.sdk.openadsdk.TTAdConfig;
 import com.bytedance.sdk.openadsdk.TTAdConstant;
 import com.bytedance.sdk.openadsdk.TTAdManager;
 import com.bytedance.sdk.openadsdk.TTAdSdk;
 import com.comm.jksdk.BuildConfig;
-import com.comm.jksdk.GeekAdSdk;
 
 /**
  * 可以用一个单例来保存TTAdManager实例，在需要初始化sdk的时候调用
@@ -77,10 +75,12 @@ public class TTAdManagerHolder {
                 .debug(adsDebug) //测试阶段打开，可以通过日志排查问题，上线时去除该调用
                 .directDownloadNetworkType(TTAdConstant.NETWORK_STATE_WIFI, TTAdConstant.NETWORK_STATE_3G, TTAdConstant.NETWORK_STATE_4G) //允许直接下载的网络状态集合
                 .supportMultiProcess(false)//是否支持多进程
+
+                .keywords("清理大师，手机加速，垃圾清理，抖音，病毒查杀，手机降温，网络加速，酷我音乐，好看点，微鲤，七猫，看点快报，玩赚星球，赚钱，安全卫士，助手，管家，西瓜视频，映客，今日头条，趣头条，火山小视频，成语大富豪，球球视频，波波视频，爱奇艺，拼多多，小说，快手，哔哩哔哩，知乎")
                 //.httpStack(new MyOkStack3())//自定义网络库，demo中给出了okhttp3版本的样例，其余请自行开发或者咨询工作人员。
                 .build();
     }
-
+/*             .gender(TTAdConstant.GENDER_MALE)  //男性*/
     public static boolean issInit() {
         return sInit;
     }
