259b8aab65d29138c9fa5ba7a95d73c4321b1273
lvdongdong <lvdongdong@hellogeek.com>
2020-03-13 18:43:01 +0800

    [fixbug

--- app/src/main/java/com/xiaoniu/cleanking/keeplive/service/RemoteService.java
+++ app/src/main/java/com/xiaoniu/cleanking/keeplive/service/RemoteService.java
@@ -65,8 +65,14 @@ public final class RemoteService extends Service {
             KeepAliveConfig.DEF_ICONS = SPUtils.getInstance(getApplicationContext(), SP_NAME).getInt(KeepAliveConfig.RES_ICON, R.drawable.ic_launcher);
             KeepAliveConfig.TITLE = SPUtils.getInstance(getApplicationContext(), SP_NAME).getString(KeepAliveConfig.TITLE);
             String title = SPUtils.getInstance(getApplicationContext(), SP_NAME).getString(KeepAliveConfig.TITLE);
-            Log.d("JOB-->"+TAG,"KeepAliveConfig.CONTENT_"+ KeepAliveConfig.CONTENT+"    " + KeepAliveConfig.TITLE+"  "+title);
-            if (!TextUtils.isEmpty(KeepAliveConfig.TITLE) && !TextUtils.isEmpty( KeepAliveConfig.CONTENT)) {
+            Log.d("JOB-->" + TAG, "KeepAliveConfig.CONTENT_" + KeepAliveConfig.CONTENT + "    " + KeepAliveConfig.TITLE + "  " + title);
+            if (TextUtils.isEmpty(KeepAliveConfig.TITLE)) {
+                KeepAliveConfig.TITLE = getString(R.string.push_content_default_title);
+            }
+            if (TextUtils.isEmpty(KeepAliveConfig.CONTENT)) {
+                KeepAliveConfig.CONTENT = getString(R.string.push_content_default_content);
+            }
+            if (!TextUtils.isEmpty(KeepAliveConfig.TITLE) && !TextUtils.isEmpty(KeepAliveConfig.CONTENT)) {
                 //启用前台服务，提升优先级
                 Intent intent2 = new Intent(getApplicationContext(), NotificationClickReceiver.class);
                 intent2.setAction(NotificationClickReceiver.CLICK_NOTIFICATION);
