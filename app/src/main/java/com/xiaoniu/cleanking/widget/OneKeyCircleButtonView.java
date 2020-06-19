88e7cf97301c7d5489306492377198ba8856d7f7
zhengzhihao <zhengzhihao@hellogeek.com>
2020-06-19 13:44:12 +0800

    bug修复；

--- app/src/main/java/com/xiaoniu/cleanking/widget/OneKeyCircleButtonView.java
+++ app/src/main/java/com/xiaoniu/cleanking/widget/OneKeyCircleButtonView.java
@@ -190,10 +190,13 @@ public class OneKeyCircleButtonView extends RelativeLayout {
 
     //清理完成狀態
     public void setClendedState(CountEntity countEntity) {
-        tv_file_total_size.setVisibility(GONE);
-        tv_file_total_tag.setText(getContext().getResources().getString(R.string.home_top_pop02_tag, countEntity.getResultSize()));
-        tv_file_total_tag.setVisibility(VISIBLE);
-        greenState(true);
+        if (null != countEntity) {
+            tv_file_total_size.setVisibility(GONE);
+            tv_file_total_tag.setText(getContext().getResources().getString(R.string.home_top_pop02_tag, countEntity.getResultSize()));
+            tv_file_total_tag.setVisibility(VISIBLE);
+            greenState(true);
+        }
+
     }
 
     //扫描完毕
