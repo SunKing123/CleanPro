efd1e1cf27caa783f68c17bd9dd0d2918469e4e6
zhengzhihao <zhengzhihao@hellogeek.com>
2020-07-05 11:49:40 +0800

    数美SDk接入： 接口appkey配置；

--- app/src/main/java/com/xiaoniu/cleanking/ui/main/presenter/MainPresenter.java
+++ app/src/main/java/com/xiaoniu/cleanking/ui/main/presenter/MainPresenter.java
@@ -982,29 +982,25 @@ public class MainPresenter extends RxPresenter<MainActivity, MainModel> implemen
         }
     }
 
-    //todo
+    /**
+     * 数美SDK初始化；
+     */
     public void initShuMeiSDK() {
         // 如果 AndroidManifest.xml 中没有指定主进程名字，主进程名默认与 packagename 相同
         if (AppApplication.getInstance().getPackageName().equals(SystemUtils.getProcessName(AppApplication.getInstance()))) {
             SmAntiFraud.SmOption option = new SmAntiFraud.SmOption();
-            String DEBUG_ORG = "FYe06ziad8tPJmiAIJP2";// organization 代码 不要传 AccessKey
-            option.setOrganization(DEBUG_ORG);
+
+            //1.通用配置项
+            option.setOrganization(Constant.SMANTIFRAUD_ORGANIZATION); //必填，组织标识，邮件中 organization 项
+            option.setAppId(Constant.APPLICATION_IDENTIFICATION); //必填，应用标识，登录数美后台应用管理查看
+            option.setPublicKey(Constant.SMANTIFRAUD_PUBLIC_KEY); //必填，加密 KEY，邮件中 android_public_key 附件内容
+            option.setAinfoKey(Constant.SMANTIFRAUD_AINFOKEY); //必填，加密 KEY，邮件中 Android ainfo key 项
             option.setChannel(ChannelUtil.getChannel());//渠道代码
-            SmAntiFraud.create(AppApplication.getInstance(), option);
-            // 注意！！获取 deviceId，这个接口在需要使用 deviceId 时地方调用。
-
-
-            //todo:zzh
-/*            //1.通用配置项
-            option.setOrganization("xxxxxxx"); //必填，组织标识，邮件中 organization 项
-            option.setAppId("default"); //必填，应用标识，登录数美后台应用管理查看
-            option.setPublicKey("xxxxxxxx"); //必填，加密 KEY，邮件中 android_public_key 附件内容
-            option.setAinfoKey("xxxxxxxx"); //必填，加密 KEY，邮件中 Android ainfo key 项
-//2.连接海外机房特殊配置项，仅供设备数据上报海外机房客户使用
-// option.setArea(SmAntiFraud.AREA_XJP); //连接新加坡机房客户使用此选项
-// option.setArea(SmAntiFraud.AREA_FJNY); //连接美国机房客户使用此选项
-//3.SDK 初始化
-            SmAntiFraud.create(this,option);*/
+            //2.连接海外机房特殊配置项，仅供设备数据上报海外机房客户使用
+            // option.setArea(SmAntiFraud.AREA_XJP); //连接新加坡机房客户使用此选项
+            // option.setArea(SmAntiFraud.AREA_FJNY); //连接美国机房客户使用此选项
+            //3.SDK 初始化
+            SmAntiFraud.create(AppApplication.getInstance(),option);
 
         }
     }
