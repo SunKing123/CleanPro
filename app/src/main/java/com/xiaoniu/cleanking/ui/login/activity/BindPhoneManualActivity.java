1a80b6ab20238de34cc28358b167d6727a63d853
snowzhao <632105276@qq.com>
2020-07-06 20:00:26 +0800

    账户接口调试

--- app/src/main/java/com/xiaoniu/cleanking/ui/login/activity/BindPhoneManualActivity.java
+++ app/src/main/java/com/xiaoniu/cleanking/ui/login/activity/BindPhoneManualActivity.java
@@ -2,6 +2,9 @@ package com.xiaoniu.cleanking.ui.login.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -9,15 +12,22 @@ import androidx.annotation.Nullable;
 import com.jess.arms.base.BaseActivity;
 import com.jess.arms.di.component.AppComponent;
 import com.jess.arms.utils.ArmsUtils;
-
 import com.xiaoniu.cleanking.R;
+import com.xiaoniu.cleanking.ui.login.bean.BindPhoneBean;
+import com.xiaoniu.cleanking.ui.login.bean.IsPhoneBindBean;
 import com.xiaoniu.cleanking.ui.login.contract.BindPhoneManualContract;
 import com.xiaoniu.cleanking.ui.login.di.component.DaggerBindPhoneManualComponent;
 import com.xiaoniu.cleanking.ui.login.presenter.BindPhoneManualPresenter;
+import com.xiaoniu.cleanking.utils.AndroidUtil;
 import com.xiaoniu.cleanking.widget.CommonTitleLayout;
-
+import com.xiaoniu.cleanking.widget.rewrite.ClearAbleEditText;
+import com.xiaoniu.cleanking.widget.rewrite.CountDownTextView;
+import com.xiaoniu.cleanking.widget.rewrite.OnTextListener;
+import com.xiaoniu.cleanking.widget.statusbarcompat.StatusBarCompat;
+import com.xiaoniu.common.utils.ToastUtils;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 import static com.jess.arms.utils.Preconditions.checkNotNull;
 
@@ -34,9 +44,18 @@ import static com.jess.arms.utils.Preconditions.checkNotNull;
  * <a href="https://github.com/JessYanCoding/MVPArmsTemplate">模版请保持更新</a>
  * ================================================
  */
-public class BindPhoneManualActivity extends BaseActivity<BindPhoneManualPresenter> implements BindPhoneManualContract.View {
+public class BindPhoneManualActivity extends BaseActivity<BindPhoneManualPresenter> implements BindPhoneManualContract.View, OnTextListener {
     @BindView(R.id.titleLayout)
     CommonTitleLayout titleLayout;
+    @BindView(R.id.bind_phone_et)
+    ClearAbleEditText bindPhoneEt;
+    @BindView(R.id.tv_verify)
+    CountDownTextView tvVerify;
+    @BindView(R.id.input_code_et)
+    ClearAbleEditText inputCodeEt;
+    @BindView(R.id.next_tv)
+    TextView nextTv;
+
     @Override
     public void setupActivityComponent(@NonNull AppComponent appComponent) {
         DaggerBindPhoneManualComponent //如找不到该类,请编译一下项目
@@ -54,7 +73,10 @@ public class BindPhoneManualActivity extends BaseActivity<BindPhoneManualPresent
 
     @Override
     public void initData(@Nullable Bundle savedInstanceState) {
-        titleLayout.setLeftTitle("登录3").setBgColor(R.color.common_white);
+        StatusBarCompat.translucentStatusBarForImage(this, true, true);
+        titleLayout.setMiddleTitle("绑定手机号").setTitleColor(R.color.color_262626).setBgColor(R.color.common_white).setLeftBackColor(R.color.color_666666);
+        bindPhoneEt.setTextListener(this);
+        inputCodeEt.setTextListener(this);
     }
 
     @Override
@@ -82,5 +104,80 @@ public class BindPhoneManualActivity extends BaseActivity<BindPhoneManualPresent
     @Override
     public void killMyself() {
         finish();
+    }
+
+    @OnClick({R.id.next_tv, R.id.tv_verify, R.id.img_back})
+    public void onViewClicked(View view) {
+        if (AndroidUtil.isFastDoubleClick()) {
+            return;
+        }
+        switch (view.getId()) {
+            case R.id.next_tv:
+                String phone = bindPhoneEt.getText().toString().trim();
+                String code = inputCodeEt.getText().toString().trim();
+                mPresenter.phoneBind(phone, code);
+                break;
+            case R.id.tv_verify:
+                String phoneNumber = bindPhoneEt.getText().toString().trim();
+                if (checkIsPhoneNum(phoneNumber)) {
+                    mPresenter.checkPhoneBinded(phoneNumber);
+                }
+                break;
+            case R.id.img_back:
+                finish();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private boolean checkIsPhoneNum(String phoneNumber) {
+        if (TextUtils.isEmpty(phoneNumber)) {
+            return false;
+        }
+        if (!phoneNumber.startsWith("1")) {
+            return false;
+        }
+        if (phoneNumber.length() != 11) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onTextAfter() {
+        if (!TextUtils.isEmpty(bindPhoneEt.getText().toString()) && !TextUtils.isEmpty(inputCodeEt.getText().toString())) {
+            nextTv.setEnabled(true);
+        } else {
+            nextTv.setEnabled(false);
+        }
+    }
+
+    @Override
+    public void getBindPhoneSuccess(BindPhoneBean listBean) {
+        ToastUtils.showShort("绑定成功");
+        finish();
+    }
+
+    String isBinded = "-1";
+
+    @Override
+    public void getIsPhoneBindedSuccess(IsPhoneBindBean listBean) {
+        //绑定
+        isBinded = listBean.getData().getIsBinded();
+        if ("1".equals(isBinded)) {
+            ToastUtils.showShort("该手机号已被绑定");
+        } else if ("0".equals(isBinded)) {
+            //未綁定 获取验证码
+            String phone = bindPhoneEt.getText().toString().trim();
+            mPresenter.getCode(phone);
+            inputCodeEt.setText("");
+        }
+    }
+
+    @Override
+    public void getCodeSuccess() {
+        tvVerify.startDialog();
+        ToastUtils.showShort("验证码发送成功");
     }
 }
--- app/src/main/java/com/xiaoniu/cleanking/ui/login/contract/BindPhoneManualContract.java
+++ app/src/main/java/com/xiaoniu/cleanking/ui/login/contract/BindPhoneManualContract.java
@@ -1,7 +1,13 @@
 package com.xiaoniu.cleanking.ui.login.contract;
 
-import com.jess.arms.mvp.IView;
 import com.jess.arms.mvp.IModel;
+import com.jess.arms.mvp.IView;
+import com.xiaoniu.cleanking.base.BaseEntity;
+import com.xiaoniu.cleanking.ui.login.bean.BindPhoneBean;
+import com.xiaoniu.cleanking.ui.login.bean.IsPhoneBindBean;
+
+import io.reactivex.Observable;
+import okhttp3.RequestBody;
 
 
 /**
@@ -19,10 +25,19 @@ import com.jess.arms.mvp.IModel;
 public interface BindPhoneManualContract {
     //对于经常使用的关于UI的方法可以定义到IView中,如显示隐藏进度条,和显示文字消息
     interface View extends IView {
+        void getBindPhoneSuccess(BindPhoneBean listBean);
 
+        void getIsPhoneBindedSuccess(IsPhoneBindBean listBean);
+
+        void getCodeSuccess();
     }
-    //Model层定义接口,外部只需关心Model返回的数据,无需关心内部细节,即是否使用缓存
-    interface Model extends IModel{
 
+    //Model层定义接口,外部只需关心Model返回的数据,无需关心内部细节,即是否使用缓存
+    interface Model extends IModel {
+        Observable<IsPhoneBindBean> checkPhoneBinded(String phoneNum);
+
+        Observable<BindPhoneBean> phoneBind(RequestBody body);
+
+        Observable<BaseEntity> sendMsg(RequestBody body);
     }
 }
