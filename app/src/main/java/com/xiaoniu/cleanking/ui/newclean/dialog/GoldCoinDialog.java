bdb64596bbcbe810c27625b24a411228d31ec5e6
zengbo <BBBbbb111>
2020-07-06 14:08:53 +0800

    弹窗金币

--- app/src/main/java/com/xiaoniu/cleanking/ui/newclean/dialog/GoldCoinDialog.java
+++ app/src/main/java/com/xiaoniu/cleanking/ui/newclean/dialog/GoldCoinDialog.java
@@ -35,6 +35,7 @@ import com.xiaoniu.cleanking.utils.LogUtils;
 import com.xiaoniu.cleanking.utils.anim.AnimationRotateUtils;
 import com.xiaoniu.cleanking.utils.anim.AnimationScaleUtils;
 import com.xiaoniu.cleanking.utils.anim.AnimationsContainer;
+import com.xiaoniu.common.utils.ToastUtils;
 import com.xnad.sdk.ad.entity.AdInfo;
 import com.xnad.sdk.ad.listener.AbsAdCallBack;
 
@@ -52,12 +53,12 @@ public class GoldCoinDialog {
     public static final String ADV_FIRST_PREFIX = "scratch_card_first";
     public static final String ADV_SECOND_PREFIX = "scratch_card_second";
     public static final String ADV_VIDEO_PREFIX = "scratch_card_video";
-
+    private static BaseDialog dialog;
     public static void showGoldCoinDialog(GoldCoinBean coinBean) {
         if (coinBean == null || coinBean.obtainCoinCount < 0) {
             return;
         }
-        BaseDialog dialog = new BaseDialog(coinBean.context, R.style.common_dialog_style);
+        dialog = new BaseDialog(coinBean.context, R.style.common_dialog_style);
         dialog.setContentView(R.layout.gold_coin_dialog);
         dialog.setCancelable(false);
         dialog.setCanceledOnTouchOutside(false);
@@ -166,6 +167,26 @@ public class GoldCoinDialog {
                 .setAdId(isVideo ? coinBean.adVideoId : coinBean.adId).setActivity((Activity) coinBean.context)
                 .setViewContainer(mRootRL).build();
         MidasRequesCenter.requestAd(params, new AbsAdCallBack() {
+
+
+            @Override
+            public void onAdError(AdInfo adInfo, int i, String s) {
+                super.onAdError(adInfo, i, s);
+                ToastUtils.showLong("网络异常");
+                if (dialog!=null){
+                    dialog.dismiss();
+                }
+            }
+
+            @Override
+            public void onShowError(int i, String s) {
+                super.onShowError(i, s);
+                ToastUtils.showLong("网络异常");
+                if (dialog!=null){
+                    dialog.dismiss();
+                }
+            }
+
             @Override
             public void onAdVideoComplete(AdInfo adInfo) {
                 super.onAdVideoComplete(adInfo);
@@ -174,6 +195,12 @@ public class GoldCoinDialog {
                     intent.putExtra(GoldCoinSuccessActivity.COIN_NUM, coinBean.obtainCoinCount * 2);
                     intent.putExtra(GoldCoinSuccessActivity.AD_ID, MidasConstants.GET_DOUBLE_GOLD_COIN_SUCCESS);
                     coinBean.context.startActivity(intent);
+                }else {
+                    ToastUtils.showLong("网络异常");
+                    if (dialog!=null){
+                        dialog.dismiss();
+                    }
+
                 }
             }
         });
