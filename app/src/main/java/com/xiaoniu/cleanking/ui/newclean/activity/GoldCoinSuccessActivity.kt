3e484e749299c523fe0d0955badea296f3aafb39
xinxiaolong <xinxiaolong@xiaoniuhy.com>
2020-07-06 11:38:27 +0800

    金币翻倍页面添加金币信息

--- app/src/main/java/com/xiaoniu/cleanking/ui/newclean/activity/GoldCoinSuccessActivity.kt
+++ app/src/main/java/com/xiaoniu/cleanking/ui/newclean/activity/GoldCoinSuccessActivity.kt
@@ -2,6 +2,11 @@ package com.xiaoniu.cleanking.ui.newclean.activity
 
 import android.content.Context
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.AbsoluteSizeSpan
+import android.widget.ImageView
+import android.widget.TextView
 import com.xiaoniu.cleanking.R
 import com.xiaoniu.cleanking.base.AppHolder
 import com.xiaoniu.cleanking.midas.AdRequestParams
@@ -14,6 +19,7 @@ import com.xnad.sdk.ad.listener.AbsAdCallBack
 import kotlinx.android.synthetic.main.activity_finish_layout.btnLeft
 import kotlinx.android.synthetic.main.activity_gold_coin_success.*
 import org.jetbrains.anko.intentFor
+import java.lang.StringBuilder
 
 
 /**
@@ -22,6 +28,11 @@ import org.jetbrains.anko.intentFor
  *  Describe: 领取金币成功页面
  */
 class GoldCoinSuccessActivity : BaseActivity() {
+
+    val COIN_NUM = "coinNum";
+
+    private var coinNum: Int = 0
+
     companion object {
         @JvmStatic
         fun start(context: Context) {
@@ -38,6 +49,17 @@ class GoldCoinSuccessActivity : BaseActivity() {
     }
 
     override fun initData() {
+        coinNum = intent.getIntExtra(COIN_NUM, 0);
+        var text="到账"+coinNum+"金币";
+
+        var sp=SpannableString(text)
+        var start=text.indexOf(coinNum.toString())
+        var end=start+coinNum.toString().length;
+        var size= AbsoluteSizeSpan(resources.getDimensionPixelSize(R.dimen.dp_24))
+        sp.setSpan(size,start,end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        tv_coin_content.setText(sp)
+
         btnLeft.setOnClickListener { finish() }
 
         //显示广告
