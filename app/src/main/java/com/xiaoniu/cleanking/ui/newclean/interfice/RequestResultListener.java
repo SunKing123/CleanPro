eddcf75d4146081e142eb93f2bbaaac24d8b359b
snowzhao <632105276@qq.com>
2020-07-14 14:28:49 +0800

    添加账号验证

---
+++ app/src/main/java/com/xiaoniu/cleanking/ui/newclean/interfice/RequestResultListener.java
@@ -0,0 +1,12 @@
+package com.xiaoniu.cleanking.ui.newclean.interfice;
+
+/**
+ * Created by zhaoyingtao
+ * Date: 2020/7/14
+ * Describe:请求结果回调接口
+ */
+public interface RequestResultListener {
+    void requestSuccess();
+
+    void requestFail();
+}
