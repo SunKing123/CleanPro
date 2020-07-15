38d7bea3c339e4882203ae11e97ac5a700da8bd5
zengbo <BBBbbb111>
2020-07-15 18:37:14 +0800

    bug fix

--- app/src/main/java/com/xiaoniu/cleanking/ui/lockscreen/PopLayerActivity.java
+++ app/src/main/java/com/xiaoniu/cleanking/ui/lockscreen/PopLayerActivity.java
@@ -3,6 +3,7 @@ package com.xiaoniu.cleanking.ui.lockscreen;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -27,7 +28,7 @@ import com.xnad.sdk.ad.listener.AbsAdCallBack;
  * @mail：zhengzhihao@hellogeek.com 弹出层Activity
  */
 public class PopLayerActivity extends AppCompatActivity implements View.OnClickListener {
-    RelativeLayout flayoutAdContainer;
+    LinearLayout adContainer;
     RelativeLayout full_screen_insert_ad_header_layout;
     //private TextView adShowTime;
     private TextView progree_tv;
@@ -38,12 +39,13 @@ public class PopLayerActivity extends AppCompatActivity implements View.OnClickL
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        LogUtils.e("==========进入了PopLayerActivity");
         StatisticsUtils.customTrackEvent("lock_screen_page_custom", "锁屏页面创建时", "", "lock_screen_page");
         StatisticsUtils.customTrackEvent("ad_request_sdk", "外部插屏发起请求", "", "external_advertising_page");
         ActivityCollector.addActivity(this, PopLayerActivity.class);
         setContentView(R.layout.activity_pop_layer);
         //adShowTime = findViewById(R.id.full_screen_insert_ad_show_time_txt);
-        flayoutAdContainer = (RelativeLayout) findViewById(R.id.flayout_ad_container);
+        adContainer = (LinearLayout) findViewById(R.id.ad_container);
         full_screen_insert_ad_header_layout = (RelativeLayout) findViewById(R.id.full_screen_insert_ad_header_layout);
         adClose = findViewById(R.id.full_screen_insert_ad_close);
         progree_tv = findViewById(R.id.progree_tv);
@@ -76,9 +78,15 @@ public class PopLayerActivity extends AppCompatActivity implements View.OnClickL
 
     public void adInit() {
         StatisticsUtils.customADRequest("ad_request", "广告请求", "1", " ", " ", "all_ad_request", "external_advertising_page", "external_advertising_page");
-        AdRequestParams params = new AdRequestParams.Builder().setViewContainer(flayoutAdContainer)
-                .setActivity(this).setAdId(MidasConstants.SCREEN_ON_ID).build();
+        AdRequestParams params = new AdRequestParams.Builder().setViewContainer(adContainer)
+                .setActivity(this).setViewWidthOffset(56).setAdId(MidasConstants.SCREEN_ON_ID).build();
         MidasRequesCenter.requestAd(params, new AbsAdCallBack() {
+            @Override
+            public void onReadyToShow(com.xnad.sdk.ad.entity.AdInfo adInfo) {
+                super.onReadyToShow(adInfo);
+                full_screen_insert_ad_header_layout.setVisibility(View.VISIBLE);
+            }
+
             @Override
             public void onAdShow(com.xnad.sdk.ad.entity.AdInfo adInfo) {
                 super.onAdShow(adInfo);
@@ -86,9 +94,9 @@ public class PopLayerActivity extends AppCompatActivity implements View.OnClickL
             }
 
             @Override
-            public void onAdError(com.xnad.sdk.ad.entity.AdInfo adInfo, int i, String s) {
-                super.onAdError(adInfo, i, s);
-                LogUtils.e("===========桌面弹窗展示失败:" + s);
+            public void onAdClose(com.xnad.sdk.ad.entity.AdInfo adInfo) {
+                super.onAdClose(adInfo);
+                finish();
             }
 
             @Override
