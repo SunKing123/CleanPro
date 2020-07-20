2e9b31468802cd0056218af6ca87b672c22c391b
zhengzhihao <zhengzhihao@hellogeek.com>
2020-07-20 14:55:35 +0800

    bug修复；

--- app/src/main/java/com/xiaoniu/cleanking/ui/main/presenter/MainPresenter.java
+++ app/src/main/java/com/xiaoniu/cleanking/ui/main/presenter/MainPresenter.java
@@ -1034,23 +1034,19 @@ public class MainPresenter extends RxPresenter<MainActivity, MainModel> implemen
     public void initShuMeiSDK() {
         // 如果 AndroidManifest.xml 中没有指定主进程名字，主进程名默认与 packagename 相同
         if (AppApplication.getInstance().getPackageName().equals(SystemUtils.getProcessName(AppApplication.getInstance()))) {
-            AppLifecyclesImpl.postDelay(new Runnable() {
-                @Override
-                public void run() {
-                    SmAntiFraud.SmOption option = new SmAntiFraud.SmOption();
-                    //1.通用配置项
-                    option.setOrganization(Constant.SMANTIFRAUD_ORGANIZATION); //必填，组织标识，邮件中 organization 项
-                    option.setAppId(Constant.APPLICATION_IDENTIFICATION); //必填，应用标识，登录数美后台应用管理查看
-                    option.setPublicKey(Constant.SMANTIFRAUD_PUBLIC_KEY); //必填，加密 KEY，邮件中 android_public_key 附件内容
-                    option.setAinfoKey(Constant.SMANTIFRAUD_AINFOKEY); //必填，加密 KEY，邮件中 Android ainfo key 项
-                    option.setChannel(ChannelUtil.getChannel());//渠道代码
-                    //2.连接海外机房特殊配置项，仅供设备数据上报海外机房客户使用
-                    // option.setArea(SmAntiFraud.AREA_XJP); //连接新加坡机房客户使用此选项
-                    // option.setArea(SmAntiFraud.AREA_FJNY); //连接美国机房客户使用此选项
-                    //3.SDK 初始化
-                    SmAntiFraud.create(AppApplication.getInstance(), option);
-                }
-            }, 2000);
+
+            SmAntiFraud.SmOption option = new SmAntiFraud.SmOption();
+            //1.通用配置项
+            option.setOrganization(Constant.SMANTIFRAUD_ORGANIZATION); //必填，组织标识，邮件中 organization 项
+            option.setAppId(Constant.APPLICATION_IDENTIFICATION); //必填，应用标识，登录数美后台应用管理查看
+            option.setPublicKey(Constant.SMANTIFRAUD_PUBLIC_KEY); //必填，加密 KEY，邮件中 android_public_key 附件内容
+            option.setAinfoKey(Constant.SMANTIFRAUD_AINFOKEY); //必填，加密 KEY，邮件中 Android ainfo key 项
+            option.setChannel(ChannelUtil.getChannel());//渠道代码
+            //2.连接海外机房特殊配置项，仅供设备数据上报海外机房客户使用
+            // option.setArea(SmAntiFraud.AREA_XJP); //连接新加坡机房客户使用此选项
+            // option.setArea(SmAntiFraud.AREA_FJNY); //连接美国机房客户使用此选项
+            //3.SDK 初始化
+            SmAntiFraud.create(AppApplication.getInstance(), option);
 
 
         }
