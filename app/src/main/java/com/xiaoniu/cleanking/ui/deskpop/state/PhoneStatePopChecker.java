e8d421801bbd833221bf27b3ff3e6a9ef748e109
xinxiaolong <xinxiaolong@xiaoniuhy.com>
2020-07-27 14:40:00 +0800

    手机状态弹框添加注释

--- app/src/main/java/com/xiaoniu/cleanking/ui/deskpop/state/PhoneStatePopChecker.java
+++ app/src/main/java/com/xiaoniu/cleanking/ui/deskpop/state/PhoneStatePopChecker.java
@@ -45,28 +45,25 @@ public class PhoneStatePopChecker implements BackGroundIPulseObserver {
 
     //条件判断
     private boolean needPop() {
+        //当运行内存使用占比≥70%时
         EasyMemoryMod easyMemoryMod = new EasyMemoryMod(AppApplication.getInstance());
         double total = easyMemoryMod.getTotalRAM();
         double available = easyMemoryMod.getAvailableRAM();
         double percent = (total / available) * 100;
-
-        //当运行内存使用占比≥70%时
         if (percent >= 70) {
             return true;
         }
 
+        //当内部存储使用占比≥70%时
         total = easyMemoryMod.getTotalInternalMemorySize();
         available = easyMemoryMod.getAvailableInternalMemorySize();
         percent = (available / total) * 100;
-
-        //当内部存储使用占比≥70%时
         if (percent >= 70) {
             return true;
         }
 
-        EasyBatteryMod easyBatteryMod = new EasyBatteryMod(AppApplication.getInstance());
-
         //当电池电量＜20%
+        EasyBatteryMod easyBatteryMod = new EasyBatteryMod(AppApplication.getInstance());
         if (easyBatteryMod.getBatteryPercentage() < 20) {
             return true;
         }
