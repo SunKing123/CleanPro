7574130effe62a4d36b81a5cc7425aaa8222511a
zengbo <BBBbbb111>
2020-07-27 11:14:42 +0800

    wifi外部插屏弹框

--- app/src/main/java/com/xiaoniu/cleanking/ui/main/activity/MainActivity.java
+++ app/src/main/java/com/xiaoniu/cleanking/ui/main/activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.xiaoniu.cleanking.ui.main.activity;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.PendingIntent;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -747,7 +748,12 @@ public class MainActivity extends BaseActivity<MainPresenter> {
         screenIntent.addFlags(Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT);
         screenIntent.addFlags(Intent.FLAG_ACTIVITY_NO_USER_ACTION);
         screenIntent.putExtra(ExternalSceneActivity.SCENE,ExternalSceneActivity.SCENE_WIFI);
-        startActivity(screenIntent);
+        PendingIntent intent=PendingIntent.getActivity(this,0,screenIntent,PendingIntent.FLAG_UPDATE_CURRENT);
+        try {
+            intent.send();
+        } catch (PendingIntent.CanceledException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
--- app/src/main/java/com/xiaoniu/cleanking/ui/newclean/activity/ExternalSceneActivity.kt
+++ app/src/main/java/com/xiaoniu/cleanking/ui/newclean/activity/ExternalSceneActivity.kt
@@ -1,8 +1,12 @@
 package com.xiaoniu.cleanking.ui.newclean.activity
 
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.net.wifi.WifiInfo
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowManager
 import com.xiaoniu.clean.deviceinfo.EasyNetworkMod
 import com.xiaoniu.cleanking.R
 import com.xiaoniu.cleanking.app.getStringExtraNotNull
@@ -10,8 +14,12 @@ import com.xiaoniu.cleanking.app.injector.component.ActivityComponent
 import com.xiaoniu.cleanking.base.BaseActivity
 import com.xiaoniu.cleanking.scheme.utils.ActivityCollector
 import com.xiaoniu.cleanking.ui.localpush.PopPushActivity
+import com.xiaoniu.cleanking.ui.main.activity.NetWorkActivity
 import com.xiaoniu.cleanking.ui.newclean.presenter.ExternalScenePresenter
+import com.xiaoniu.cleanking.ui.viruskill.VirusKillActivity
 import com.xiaoniu.cleanking.utils.NumberUtils
+import com.xiaoniu.cleanking.utils.update.PreferenceUtil
+import com.xiaoniu.cleanking.widget.statusbarcompat.StatusBarCompat
 import kotlinx.android.synthetic.main.activity_external_scene.*
 import kotlinx.android.synthetic.main.view_scene_wifi.*
 
@@ -22,8 +30,18 @@ class ExternalSceneActivity : BaseActivity<ExternalScenePresenter>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            StatusBarCompat.setStatusBarColor(this, resources.getColor(android.R.color.transparent, theme), true)
+        } else {
+            StatusBarCompat.setStatusBarColor(this, resources.getColor(android.R.color.transparent), false)
+        }
         window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         ActivityCollector.addActivity(this, PopPushActivity::class.java)
+        window.addFlags(
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                        or WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                //  or WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
+        )
     }
 
     override fun getLayoutId() = R.layout.activity_external_scene
@@ -54,12 +72,39 @@ class ExternalSceneActivity : BaseActivity<ExternalScenePresenter>() {
             }
         }
         scene_close.setOnClickListener { finish() }
+
+    }
+
+    private fun startNetSpeed() {
+        if (PreferenceUtil.getSpeedNetWorkTime()) {
+            startActivity(NetWorkActivity::class.java)
+        } else {
+            val intent = Intent(this, NewCleanFinishActivity::class.java)
+            val num = PreferenceUtil.getSpeedNetworkValue()
+            intent.putExtra("title", "网络加速")
+            intent.putExtra("main", false)
+            intent.putExtra("num", num)
+            intent.putExtra("unused", true)
+            startActivity(intent)
+        }
+    }
+
+    private fun startKillVirusActivity() {
+        if (PreferenceUtil.getVirusKillTime()) {
+            startActivity(VirusKillActivity::class.java)
+        } else {
+            val intent = Intent(this, NewCleanFinishActivity::class.java)
+            intent.putExtra("title", "病毒查杀")
+            intent.putExtra("main", false)
+            intent.putExtra("unused", true)
+            startActivity(intent)
+        }
     }
 
     private fun showWifiScene() {
         scene_title.text = "WIFI已经连接"
-        scene_button.text = "网络加速"
         val easyNetworkMod = EasyNetworkMod(this)
+        scene_button.text = "网络安全检测"
         //wifi图标
         when (easyNetworkMod.checkWifiState()) {
             1 -> {
@@ -73,7 +118,16 @@ class ExternalSceneActivity : BaseActivity<ExternalScenePresenter>() {
             3 -> {
                 wifi_image.setImageResource(R.mipmap.wifi_low)
                 wifi_state.text = "弱"
+                scene_button.text = "网络加速"
             }
+        }
+        scene_button.setOnClickListener {
+            if (wifi_state.text.contains("弱")) {
+                startNetSpeed()
+            } else {
+                startKillVirusActivity()
+            }
+            finish()
         }
         //名称
         wifi_name.text = easyNetworkMod.wifiSSID
@@ -81,7 +135,7 @@ class ExternalSceneActivity : BaseActivity<ExternalScenePresenter>() {
         wifi_open_type.text = if (easyNetworkMod.checkIsCurrentWifiHasPassword()) "隐私" else "开放"
         //网络速度
         wifi_net_speed.text = easyNetworkMod.wifiLinkSpeed
-        if (easyNetworkMod.wifiLinkSpeed.contains("Mbps")) {
+        if (easyNetworkMod.wifiLinkSpeed.contains(WifiInfo.LINK_SPEED_UNITS)) {
             val speed = easyNetworkMod.wifiLinkSpeed.replace("Mbps", "").trim().toInt()
             //下载速度
             wifi_download_speed.text = NumberUtils.mathRandom(speed / 2, speed).toString().plus("Mbps")
