408a750d55c42f81e6492d4134a0b82211144026
xinxiaolong <xinxiaolong@xiaoniuhy.com>
2020-07-27 13:54:24 +0800

    监测弹框条件

--- app/src/main/java/com/xiaoniu/cleanking/ui/external/PhoneStatePopChecker.java
+++ app/src/main/java/com/xiaoniu/cleanking/ui/external/PhoneStatePopChecker.java
@@ -1,6 +1,8 @@
 package com.xiaoniu.cleanking.ui.external;
 
 import com.jess.arms.base.App;
+import com.xiaoniu.clean.deviceinfo.EasyBatteryMod;
+import com.xiaoniu.clean.deviceinfo.EasyMemoryMod;
 import com.xiaoniu.cleanking.app.AppApplication;
 import com.xiaoniu.cleanking.utils.rxjava.BackGroundPulseTimer;
 import com.xiaoniu.cleanking.utils.rxjava.BackGroundIPulseObserver;
@@ -10,30 +12,67 @@ import com.xiaoniu.common.utils.ToastUtils;
  * Created by xinxiaolong on 2020/7/25.
  * email：xinxiaolong123@foxmail.com
  */
-public class PhoneStatePopChecker implements BackGroundIPulseObserver{
+public class PhoneStatePopChecker implements BackGroundIPulseObserver {
 
     long startTime;
 
     @Override
     public void onCreate() {
-        startTime=System.currentTimeMillis();
+        startTime = System.currentTimeMillis();
     }
 
     @Override
     public void onPulse(long progress) {
-        long diff=System.currentTimeMillis()-startTime;
-        diff=diff/1000;
-        if(diff>10){
+        long diff = System.currentTimeMillis() - startTime;
+        diff = diff / 1000;
+        diff = diff / 60;
+        if (diff > 30) {
             checkAndPop();
         }
     }
 
-    private void checkAndPop(){
-        ExternalPhoneStateActivity.start(AppApplication.getInstance());
+    private void checkAndPop() {
+        if (needPop())
+            ExternalPhoneStateActivity.start(AppApplication.getInstance());
     }
 
     @Override
     public void onDestroy() {
 
+    }
+
+    private boolean needPop() {
+        EasyMemoryMod easyMemoryMod = new EasyMemoryMod(AppApplication.getInstance());
+        double total = easyMemoryMod.getTotalRAM();
+        double available = easyMemoryMod.getAvailableRAM();
+        double percent = (total / available) * 100;
+
+        //当运行内存使用占比≥70%时
+        if (percent >= 70) {
+            return true;
+        }
+
+        total = easyMemoryMod.getTotalInternalMemorySize();
+        available = easyMemoryMod.getAvailableInternalMemorySize();
+        percent = (available / total) * 100;
+
+        //当内部存储使用占比≥70%时
+        if (percent >= 70) {
+            return true;
+        }
+
+        EasyBatteryMod easyBatteryMod = new EasyBatteryMod(AppApplication.getInstance());
+
+        //当电池电量＜20%
+        if (easyBatteryMod.getBatteryPercentage() <= 20) {
+            return true;
+        }
+
+        //当电池温度＞37°
+        if (easyBatteryMod.getBatteryTemperature() > 37) {
+            return true;
+        }
+
+        return false;
     }
 }
