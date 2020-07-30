b00bbb7d26f2c2ce5fcf29c4bf0bcd4dbca46b7d
zengbo <BBBbbb111>
2020-07-30 10:28:15 +0800

    wifi弹窗

--- app/src/main/java/com/xiaoniu/cleanking/ui/main/presenter/PhoneAccessPresenter.java
+++ app/src/main/java/com/xiaoniu/cleanking/ui/main/presenter/PhoneAccessPresenter.java
@@ -242,7 +242,7 @@ public class PhoneAccessPresenter extends RxPresenter<PhoneAccessActivity, MainM
     boolean canPlaying = true;
 
     public void setNumAnim(View view, TextView tv_size, TextView tv_size_show, TextView tv_delete, TextView tv_gb, TextView tv_gb1, View viewt, View view_top, int startNum, int endNum, int type) {
-        ArrayList<FirstJunkInfo> installAppList = AndroidUtil.getRandomMaxCountInstallApp(mActivity, 30);
+        ArrayList<FirstJunkInfo> installAppList = AndroidUtil.getRandomMaxCountInstallApp(mActivity, 20);
         //第三方应用不足5个从系统应用中拿
         if (installAppList.size() < 5) {
             installAppList.addAll(AndroidUtil.getSystemInstallApps(mActivity, 10));
