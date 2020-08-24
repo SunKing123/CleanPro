fe61508887c77bcb84a6db99f9d690391263776b
zhengzhihao <zhengzhihao1@126.com>
2020-08-24 12:43:38 +0800

    关键字修改；

--- app/src/main/java/com/xiaoniu/cleanking/ui/main/activity/NewsLoadActivity.java
+++ app/src/main/java/com/xiaoniu/cleanking/ui/main/activity/NewsLoadActivity.java
@@ -77,7 +77,7 @@ public class NewsLoadActivity extends SimpleActivity implements MainActivity.OnK
                 return;
             }
             if (!isDestroyed()) {
-                mCanGoBack = !"清理管家极速版商城".equals(title);
+                mCanGoBack = !"清理极速管家商城".equals(title);
             }
             if (mTextTitle != null) {
                 mTextTitle.setText("新闻热点");
