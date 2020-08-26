d8f7266dfc27a10153b69de16c97e3a2e5192f9b
xinxiaolong <xinxiaolong@xiaoniuhy.com>
2020-08-26 17:09:04 +0800

    固定高度的gridview

---
+++ app/src/main/java/com/xiaoniu/cleanking/ui/securitycenter/entrance/view/FixRowGridView.kt
@@ -0,0 +1,18 @@
+package com.xiaoniu.cleanking.ui.securitycenter.entrance.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.GridView
+
+/**
+ * Created by xinxiaolong on 2020/8/26.
+ * email：xinxiaolong123@foxmail.com
+ */
+class FixRowGridView(context: Context,attrs: AttributeSet) :GridView(context,attrs){
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val expandSpec = MeasureSpec.makeMeasureSpec(
+                Int.MAX_VALUE shr 2, MeasureSpec.AT_MOST)
+        super.onMeasure(widthMeasureSpec, expandSpec)
+    }
+}
