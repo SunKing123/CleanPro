57e5ce30e6bae95551740a52f02c567b9df907ca
xinxiaolong <xinxiaolong@xiaoniuhy.com>
2020-09-03 17:40:19 +0800

    安全中心首页功能条布局封装。

---
+++ app/src/main/java/com/xiaoniu/cleanking/ui/securitycenter/entrance/view/FunctionBarView.kt
@@ -0,0 +1,18 @@
+package com.xiaoniu.cleanking.ui.securitycenter.entrance.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.xiaoniu.cleanking.R
+
+/**
+ * Created by xinxiaolong on 2020/9/3.
+ * email：xinxiaolong123@foxmail.com
+ */
+class FunctionBarView(context: Context, attrs: AttributeSet) : ConstraintLayout(context, attrs) {
+
+    var mView: View = LayoutInflater.from(context).inflate(R.layout.view_security_home_function_bar_layout, this, true)
+
+}
